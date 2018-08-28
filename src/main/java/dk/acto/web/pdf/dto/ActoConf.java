54c408ce6f42109a61459d5c53e439d49e3004f5
Mikkel L?kke <ml@acto.dk>
2018-08-28 13:14:29 +0200

    Added Jackson annotations to support serialization of ActoConf.

--- src/main/java/dk/acto/web/pdf/dto/ActoConf.java
+++ src/main/java/dk/acto/web/pdf/dto/ActoConf.java
@@ -1,10 +1,13 @@
 package dk.acto.web.pdf.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonDeserialize(builder = ActoConf.ActoConfBuilder.class)
 public class ActoConf {
     public static final ActoConf DEFAULT = ActoConf.builder()
             .enabledGet(true)
@@ -15,4 +18,9 @@ public class ActoConf {
     private final boolean enabledGet;
     private final boolean enabledPost;
     private final String secret;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static final class ActoConfBuilder {
+    }
+
 }
